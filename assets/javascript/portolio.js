9c032fbde8e77b5ab70585088cb811295503bf94
Julia Finarovsky <juliafinarovsky@gmail.com>
2017-04-18 23:59:08 -0400

    updated css, added highlights for navbar, added accordions

--- assets/javascript/portolio.js
+++ assets/javascript/portolio.js
@@ -2,19 +2,19 @@ var portfolioState = {
   currentQuote: 0,
   initial: 0,
   quotes: [
-      {
-        quote: `Choose a job you Love, and you will never have to work a day in your life`,
-        author : `~ Confucius`
-      },
-      {
-        quote:`The heart of human excellence often begins to beat when you discover a pursuit that absorbs you, frees you, challenges you, and gives you a sense of meaning, joy and passion.`,
-        author: `~ Terry Orlick`
-      },
-      {
-        quote:`“I have no special talent. I am only passionately curious.`,
-        author: `~ Albert Einstein`
-      } 
-    ] 
+    {
+      quote: `Choose a job you Love, and you will never have to work a day in your life`,
+      author : `~ Confucius`
+    },
+    {
+      quote:`The heart of human excellence often begins to beat when you discover a pursuit that absorbs you, frees you, challenges you, and gives you a sense of meaning, joy and passion.`,
+      author: `~ Terry Orlick`
+    },
+    {
+      quote:`“I have no special talent. I am only passionately curious.`,
+      author: `~ Albert Einstein`
+    } 
+  ] 
 };
 
 function renderQuote() {
@@ -28,19 +28,19 @@ function renderQuote() {
     if (portfolioState.currentQuote === 2) {
       // write quotes to state then reset quote counter
       $('.quote, .author').fadeOut(1000, function() {
-      $('.quote').html(quoteHTML);
-      $('.author').html(authorHTML);
-      $('.quote, .author').fadeIn(800);
-      portfolioState.currentQuote = 0;
+        $('.quote').html(quoteHTML);
+        $('.author').html(authorHTML);
+        $('.quote, .author').fadeIn(800);
+        portfolioState.currentQuote = 0;
       
       }); 
     } else {
 
-    portfolioState.currentQuote+=1;
+      portfolioState.currentQuote+=1;
       $('.quote, .author').fadeOut(1200, function() {
-      $('.quote').html(quoteHTML);
-      $('.author').html(authorHTML);
-      $('.quote, .author').fadeIn(1200);
+        $('.quote').html(quoteHTML);
+        $('.author').html(authorHTML);
+        $('.quote, .author').fadeIn(1200);
       });
     }
     renderQuote();
@@ -48,23 +48,22 @@ function renderQuote() {
 }
 
 function accordionListener () {
-  var allPanels = $('div.panel').hide();
-
   $('.accordion').click(function (event) {
-    
+    event.preventDefault();
 
-    if($(this).next().css('display','none')) {
-
-      $(this).next().slideDown();
-      return
-    };
-    if ($(this).next().css('display', 'block')) {
-    $(this).next().slideUp();
-    return;
-    }
-
+    $(this).next().slideToggle('slow');
+    $(this).toggleClass('minus');
   });
 };
+
+function navbarActiveListener () {
+
+  $('#nav li').click(function(event){
+ 
+    $(this).toggleClass('selected');
+    $('#nav li').not($(this)).removeClass('selected');
+  });
+}
 
 
 
@@ -76,5 +75,6 @@ function accordionListener () {
 ($(document).ready(function () {
   renderQuote();
   accordionListener();
+  navbarActiveListener();
 }));
 
